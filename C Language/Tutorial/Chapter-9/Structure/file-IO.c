5924e825fc73c080c50a4cd3f7762cd5d8d04f7f
Geetansh124 <geetanshpatle2@gmail.com>
2026-01-13 19:37:01 +0530

    main

--- C Language/Tutorial/Chapter-9/Structure/file-IO.c
+++ C Language/Tutorial/Chapter-9/Structure/file-IO.c
@@ -2,9 +2,13 @@
 
 int main()  {
     FILE *fptr;
-    fptr = fopen("Test.txt",'w');
-
+    fptr = fopen("Test.txt", "a");
+    
     fprintf(fptr, "%c", 'M');
+    fprintf(fptr, "%c", 'a');
+    fprintf(fptr, "%c", 'n');
+    fprintf(fptr, "%c", 'g');
+    fprintf(fptr, "%c", 'o');
    
     fclose(fptr);
 
